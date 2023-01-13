5449d57011404ba1ccbb12bdd379f4edb71a9a9d
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2023-01-13 16:05:28 -0700

    added navigation notes to the detail page, set up back and booking buttons

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  onBookPlace() {
+    console.log('place booked')
+    // this.router.navigateByUrl('/places/discover')
+    this.navCtrl.navigateBack('/places/discover') // This is to get the correct page transfer animation Ionic is meant to be used for
+    // this.navCtrl.pop() // This would also work, but if the app is refreshed on the details page, the "Book" button wouldn't take you
+    // anywhere since the page stack is now empty. This would be the use case if it were a native mobile app, since you can't refresh the app in the same manner. 
   }
 
 }
