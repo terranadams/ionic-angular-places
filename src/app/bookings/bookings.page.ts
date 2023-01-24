bdc3b3d628794373d21175b740a3ecfc991a9976
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2023-01-24 15:08:35 -0700

    dummy data for bookings service and displaying on bookings page

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IonItemSliding } from '@ionic/angular';
+import { Booking } from './booking.model';
+import { BookingService } from './booking.service';
 
 @Component({
   selector: 'app-bookings',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingsPage implements OnInit {
 
-  constructor() { }
+  loadedBookings!: Booking[]
+
+  constructor(private bookingsService: BookingService) { }
 
   ngOnInit() {
+    this.loadedBookings = this.bookingsService.bookings
+  }
+
+  onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
+    slidingEl.close()
+    // cancel booking with offerId code
   }
 
 }
