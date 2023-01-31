2e9379896fb1b8f57ee0387b1d9fd84fd6b8fd65
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2023-01-31 16:40:42 -0700

    subject added, nothing broke so far

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { SegmentChangeEventDetail } from '@ionic/angular';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MenuController, SegmentChangeEventDetail } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 
@@ -8,19 +9,34 @@ import { PlacesService } from '../places.service';
   templateUrl: './discover.page.html',
   styleUrls: ['./discover.page.scss'],
 })
-export class DiscoverPage implements OnInit {
+export class DiscoverPage implements OnInit, OnDestroy {
+  loadedPlaces!: Place[];
+  listedLoadedPlaces!: Place[];
+  private placesSub!: Subscription;
 
-  loadedPlaces!: Place[]
-
-  constructor(private placesService: PlacesService) { }
+  constructor(
+    private placesService: PlacesService,
+    private menuCtrl: MenuController
+  ) {}
 
   ngOnInit() {
-    // this.loadedPlaces = this.placesService.places // old logic pre subject
+    this.placesService.places.subscribe(places => {
+      this.loadedPlaces = places
+      this.listedLoadedPlaces = this.loadedPlaces.slice(1)
+    })
+  }
+
+  onOpenMenu() {
+    this.menuCtrl.toggle();
   }
 
   onFilterUpdate(event: any) {
-    console.log(event.detail)
-
+    console.log(event.detail);
   }
 
+  ngOnDestroy() {
+    if (this.placesSub) {
+      this.placesSub.unsubscribe();
+    }
+  }
 }
